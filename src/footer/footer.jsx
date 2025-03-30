e82ac7015b3e0eb49289d16df2a2997fff0f4bbb
Jonathan <jonathangager75@gmail.com>
2025-03-30 11:54:46 +0200

    Add page Footer

---
+++ src/footer/footer.jsx
@@ -0,0 +1,11 @@
+import "../footer/footer.css";
+
+function Footer() {
+  return (
+    <footer>
+      <h1>END</h1>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Header from './header/header.jsx'
+import Footer from './footer/footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
     <App />
+    <Footer />
   </StrictMode>,
 )
