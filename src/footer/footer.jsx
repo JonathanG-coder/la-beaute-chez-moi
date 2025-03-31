1c36f2f8ecd310a888af39ef1f79806ef2c8ec11
Jonathan <jonathangager75@gmail.com>
2025-03-31 15:46:23 +0200

    MAJ Style Foote

--- src/footer/footer.jsx
+++ src/footer/footer.jsx
@@ -15,7 +15,7 @@ function Footer() {
           <div className="footer-right-infos">
             <div className="footer-infos">
               <p>
-                <strong>Adresse :</strong> Espelette
+                <strong>Adresse :</strong><a href="https://www.google.com/maps/place/64250+Espelette/@43.3226866,-1.5344936,12z/data=!4m15!1m8!3m7!1s0xd513ca3d30bb459:0x158cd74bfcaa3df5!2s64250+Espelette!3b1!8m2!3d43.3411775!4d-1.4493722!16zL20vMDhkbG1x!3m5!1s0xd513ca3d30bb459:0x158cd74bfcaa3df5!8m2!3d43.3411775!4d-1.4493722!16zL20vMDhkbG1x?entry=ttu&g_ep=EgoyMDI1MDMyNS4xIKXMDSoASAFQAw%3D%3D">Espelette </a>
               </p>
               <p>
                 <strong>Telephone :</strong> 00 11 ..
