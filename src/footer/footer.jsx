93409ed1fcd35dd911ce1d4a1c998e566013fe58
Jonathan <jonathangager75@gmail.com>
2025-03-31 10:20:34 +0200

    Add infos footer

--- src/footer/footer.jsx
+++ src/footer/footer.jsx
@@ -1,9 +1,42 @@
 import "../footer/footer.css";
+import logo from "../assets/Logo-blanc.png";
 
 function Footer() {
   return (
     <footer>
-      <h1>END</h1>
+      <div className="footer-container">
+        <div className="footer-logo">
+          <img src={logo} alt="LOGO du site LA BEAUTE CHEZ MOI" />
+        </div>
+        <div className="footer-right">
+          <div className="footer-infos">
+            <p>
+              <strong>Informations : </strong>
+            </p>
+            <p>
+              <strong>Adresse :</strong> Espelette
+            </p>
+            <p>
+              <strong>Telephone :</strong> 00 11 ..
+            </p>
+            <p>
+              <strong>Horaires :</strong> Selon RDV
+            </p>
+            <p>
+              <strong>E-mail :</strong> xx@gmail.com
+            </p>
+          </div>
+        </div>
+        <div className="footer-reseaux-sociaux">
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+          ></link>
+
+          <a href="#" class="fa fa-facebook"></a>
+          <a href="#" class="fa fa-twitter"></a>
+        </div>
+      </div>
     </footer>
   );
 }
