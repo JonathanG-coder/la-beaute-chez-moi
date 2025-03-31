b8382d6bbeacbd715d3916fdc384ff34c3aeb22a
Jonathan <jonathangager75@gmail.com>
2025-03-31 11:38:29 +0200

    Modif Footer

--- src/footer/footer.jsx
+++ src/footer/footer.jsx
@@ -12,28 +12,30 @@ function Footer() {
           <td>
             <strong>Informations : </strong>
           </td>
-          <div className="footer-infos">
-            <p>
-              <strong>Adresse :</strong> Espelette
-            </p>
-            <p>
-              <strong>Telephone :</strong> 00 11 ..
-            </p>
-            <p>
-              <strong>Horaires :</strong> Selon RDV
-            </p>
-            <p>
-              <strong>E-mail :</strong> xx@gmail.com
-            </p>
-          </div>
-          <div className="footer-reseaux-sociaux">
-            <link
-              rel="stylesheet"
-              href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-            ></link>
+          <div className="footer-right-infos">
+            <div className="footer-infos">
+              <p>
+                <strong>Adresse :</strong> Espelette
+              </p>
+              <p>
+                <strong>Telephone :</strong> 00 11 ..
+              </p>
+              <p>
+                <strong>Horaires :</strong> Selon RDV
+              </p>
+              <p>
+                <strong>E-mail :</strong> xx@gmail.com
+              </p>
+            </div>
+            <div className="footer-reseaux-sociaux">
+              <link
+                rel="stylesheet"
+                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+              ></link>
 
-            <a href="#" class="fa fa-facebook"></a>
-            <a href="#" class="fa fa-twitter"></a>
+              <a href="#" class="fa fa-facebook"></a>
+              <a href="#" class="fa fa-twitter"></a>
+            </div>
           </div>
         </div>
       </div>
