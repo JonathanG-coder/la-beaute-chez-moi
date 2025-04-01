3a8908846d30b55d9a56e980fdc07b12b49499c2
Jonathan <jonathangager75@gmail.com>
2025-04-01 15:06:56 +0200

    Add Prestation at Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,37 @@
+import './home.css';
+import logo from "../../assets/Logo-blanc.png";
+import regard from "../../assets/Beaute-du-regard.jpg";
+import manucure from "../../assets/manucures.jpg";
+import massage from "../../assets/massages.jpg"
+
+
+
 function Home() {
   return (
     <div>
-      <div className="accueil-presentation">
+      {/* presentation */}
+      <div className="home-presentation">
         <h1>La Beauté Chez Moi</h1>
         <button>Prendre Rendez-vous</button>
       </div>
-      ,
-      <section className="accueil-prestations">
-        <h2>HEllo</h2>
+      {/* Prestations */}
+      <section className="home-prestations">
+        <img src={logo} alt="logo du site" />
+        <h2>Prestations</h2>
+        <div className="grid-container">
+          <div className="grid-item"><img src={regard} alt="Une femme qui se fait rehausser ses cils" /></div>
+          <div className="grid-item"><img src={manucure} alt="Une femme qui se fait faire les ongles" /></div>
+          <div className="grid-item"><img src={massage} alt="Une femme qui se fait masser le dos" /></div>
+        </div>
       </section>
+
+
+
+
+
+
+
+
     </div>
   );
 }
