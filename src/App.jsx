7d63a9667d914a8940259f7ff367fde84b50b14a
Jonathan <jonathangager75@gmail.com>
2025-04-02 13:12:23 +0200

    MAJ Responsive Header

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
-import home from "../src/pages/home/home";
-import about from "./pages/about/about";
+import Home from "./pages/home/home";
+import About from "../src/pages/about/about";
 
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<home/>} />
-        <Route path="/About" element={<about/>}/>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>}/>
         {/* <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} /> */}
       </Routes>
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,25 +1,42 @@
-import logo from "../../assets/Logo-blanc.png";
-import '../header/header.css';
+// Header.js
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { Menu, X } from "lucide-react";
+import logo from "../../assets/Logo-blanc.png";
+import "../header/header.css";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header>
       <img src={logo} alt="LOGO du site LA BEAUTE CHEZ MOI" />
-
-      {/* Menu visible sur grand écran */}
       
       <nav className="desktop-menu">
         <ul>
-        <li><Link to="/">Accueil</Link></li>
-        <li><Link to="/about">À Propos</Link></li>
-        <li><Link to="/prestations">Prestations</Link></li>
-        <li><Link to="/prendre-rendez-vous">Prendre Rendez-vous</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-      </ul>
+          <li><Link to="/">Accueil</Link></li>
+          <li><Link to="/about">À Propos</Link></li>
+          <li><Link to="/prestations">Prestations</Link></li>
+          <li><Link to="/prendre-rendez-vous">Prendre Rendez-vous</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
       </nav>
+
+      <button className="burger-menu" onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? <X size={30} /> : <Menu size={30} />}
+      </button>
+      
+      <div className={`mobile-menu ${isOpen ? "open" : ""}`}>
+        <ul>
+          <li><Link to="/" onClick={() => setIsOpen(false)}>Accueil</Link></li>
+          <li><Link to="/about" onClick={() => setIsOpen(false)}>À Propos</Link></li>
+          <li><Link to="/prestations" onClick={() => setIsOpen(false)}>Prestations</Link></li>
+          <li><Link to="/prendre-rendez-vous" onClick={() => setIsOpen(false)}>Prendre Rendez-vous</Link></li>
+          <li><Link to="/contact" onClick={() => setIsOpen(false)}>Contact</Link></li>
+        </ul>
+      </div>
     </header>
   );
 }
 
-export default Header
+export default Header;
