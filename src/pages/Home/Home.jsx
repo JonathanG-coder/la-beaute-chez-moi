22a44aa444bd55a7ceb853fe896e295a083bc199
Jonathan <jonathangager75@gmail.com>
2025-04-03 15:39:33 +0200

    MAJ responsive at Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,66 +1,74 @@
 import "./home.css";
+import { useState } from "react";
 import logo from "../../assets/Logo-blanc.png";
 import bio from "../../assets/sourcils2.jpg";
 
-function home() {
+function Home() {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   return (
     <div>
-      {/* presentation */}
+      {/* Présentation */}
       <div className="home-presentation">
         <h1>La Beauté Chez Moi</h1>
         <button>Prendre Rendez-vous</button>
       </div>
 
-      {/* biographie */}
+      {/* Biographie */}
       <div className="bio">
         <div className="bio-img">
           <img
             src={bio}
-            alt="Image d'une femme qui realise des prestation de beauté du regard"
+            alt="Image d'une femme qui réalise des prestations de beauté du regard"
           />
         </div>
-        <div className="bio-titre">
-          <p>-L'expérience-</p>
-          <h2> La beauté Chez Vous </h2>
-        </div>
-        <div className="bio-texte">
-          <p>
-            Après plusieurs années dans le domaine médical, où j'ai appris à
-            prendre soin des autres avec rigueur et bienveillance, j'ai choisi
-            de me réorienter vers le monde de la beauté. Cette reconversion m'a
-            permis de combiner mes compétences en soins et mon désir profond
-            d'aider les autres à se sentir bien dans leur peau.
-          </p>
-          <p>
-            Formée dans les domaines de l'extension et du rehaussement de cils,
-            des massages, de la manucure et de l'épilation des sourcils, je mets
-            un point d'honneur à offrir des soins de qualité, réalisés avec une
-            grande attention, pour que chaque client se sente à la fois détendu
-            et sublimé.
-          </p>
-          <p>
-            Dans un cadre intime et serein, je vous accueille avec plaisir chez
-            moi, dans un environnement sain, où chaque soin est réalisé avec des
-            produits de qualité, respectueux de votre peau et de vos besoins.
-          </p>
-          <p>
-            Mon expérience médicale m’a appris l’importance de l’écoute et du
-            respect des besoins individuels, afin que chaque moment passé ici
-            soit une véritable parenthèse de bien-être.{" "}
-          </p>
-          <p>
-            Merci de me faire confiance pour prendre soin de vous. Contactez-moi
-            pour plus d'informations ou pour réserver votre rendez-vous. J’ai
-            hâte de vous rencontrer et de vous offrir un soin personnalisé, en
-            toute tranquillité et professionnalisme.
-          </p>
+        <div className="bio-content">
+          <div className="bio-titre">
+            <p>-L'expérience-</p>
+            <h2> La beauté Chez Vous </h2>
+          </div>
+          <div className="bio-texte">
+            <p>
+              Après plusieurs années dans le domaine médical, où j'ai appris à
+              prendre soin des autres avec rigueur et bienveillance, j'ai choisi
+              de me réorienter vers le monde de la beauté.
+            </p>
+
+            {isExpanded && (
+              <>
+                <p>
+                  Cette reconversion m'a permis de combiner mes compétences en soins et mon désir profond
+                  d'aider les autres à se sentir bien dans leur peau.
+                </p>
+                <p>
+                  Formée dans les domaines de l'extension et du rehaussement de cils,
+                  des massages, de la manucure et de l'épilation des sourcils...
+                </p>
+                <p>
+                  Dans un cadre intime et serein, je vous accueille avec plaisir chez moi,
+                  où chaque soin est réalisé avec des produits de qualité.
+                </p>
+                <p>
+                  Mon expérience médicale m’a appris l’importance de l’écoute et du
+                  respect des besoins individuels, afin que chaque moment soit une véritable parenthèse de bien-être.
+                </p>
+                <p>
+                  Merci de me faire confiance ! Contactez-moi pour plus d'informations ou pour réserver votre rendez-vous.
+                </p>
+              </>
+            )}
+
+            <button className="bio-button" onClick={() => setIsExpanded(!isExpanded)}>
+              {isExpanded ? "Lire moins" : "Lire la suite"}
+            </button>
+          </div>
         </div>
       </div>
 
       {/* Prestations */}
       <section className="home-prestations">
         <img src={logo} alt="logo du site" />
-        <h2>Decouvrez mes prestations</h2>
+        <h2>Découvrez mes prestations</h2>
         <div className="grid-container">
           <div className="grid-item item1">
             <span>Beauté du regard</span>
@@ -73,9 +81,10 @@ function home() {
           </div>
         </div>
       </section>
+
       <section className="avis-client"></section>
     </div>
   );
 }
 
-export default home;
+export default Home;
