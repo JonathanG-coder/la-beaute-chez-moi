8a9e3d934bc5ecbfe8ff3cf814fee66acd733074
Jonathan <jonathangager75@gmail.com>
2025-04-03 14:58:07 +0200

    MAJ Responsive page home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -13,18 +13,49 @@ function home() {
 
       {/* biographie */}
       <div className="bio">
-        <div className="bio-img"><img src={bio} alt="Image d'une femme qui realise des prestation de beauté du regard" /></div>
-        <div className="bio-titre"><p>-L'expérience-</p><h2> La beauté Chez Vous </h2></div>
-        <div className="bio-texte"><p>Après plusieurs années dans le domaine médical, où j'ai appris à prendre soin des autres avec rigueur et bienveillance, j'ai choisi de me réorienter vers le monde de la beauté.
- Cette reconversion m'a permis de combiner mes compétences en soins et mon désir profond d'aider les autres à se sentir bien dans leur peau.</p>
-<p>Formée dans les domaines de l'extension et du rehaussement de cils, des massages, de la manucure et de l'épilation des sourcils, je mets un point d'honneur à offrir des soins de qualité, réalisés avec une grande attention, pour que chaque client se sente à la fois détendu et sublimé.</p>
-<p>Dans un cadre intime et serein, je vous accueille avec plaisir chez moi, dans un environnement sain, où chaque soin est réalisé avec des produits de qualité, respectueux de votre peau et de vos besoins.</p>
-<p>Mon expérience médicale m’a appris l’importance de l’écoute et du respect des besoins individuels, afin que chaque moment passé ici soit une véritable parenthèse de bien-être. </p>
-<p>Merci de me faire confiance pour prendre soin de vous. Contactez-moi pour plus d'informations ou pour réserver votre rendez-vous. J’ai hâte de vous rencontrer et de vous offrir un soin personnalisé, en toute tranquillité et professionnalisme.</p>
-</div>
+        <div className="bio-img">
+          <img
+            src={bio}
+            alt="Image d'une femme qui realise des prestation de beauté du regard"
+          />
+        </div>
+        <div className="bio-titre">
+          <p>-L'expérience-</p>
+          <h2> La beauté Chez Vous </h2>
+        </div>
+        <div className="bio-texte">
+          <p>
+            Après plusieurs années dans le domaine médical, où j'ai appris à
+            prendre soin des autres avec rigueur et bienveillance, j'ai choisi
+            de me réorienter vers le monde de la beauté. Cette reconversion m'a
+            permis de combiner mes compétences en soins et mon désir profond
+            d'aider les autres à se sentir bien dans leur peau.
+          </p>
+          <p>
+            Formée dans les domaines de l'extension et du rehaussement de cils,
+            des massages, de la manucure et de l'épilation des sourcils, je mets
+            un point d'honneur à offrir des soins de qualité, réalisés avec une
+            grande attention, pour que chaque client se sente à la fois détendu
+            et sublimé.
+          </p>
+          <p>
+            Dans un cadre intime et serein, je vous accueille avec plaisir chez
+            moi, dans un environnement sain, où chaque soin est réalisé avec des
+            produits de qualité, respectueux de votre peau et de vos besoins.
+          </p>
+          <p>
+            Mon expérience médicale m’a appris l’importance de l’écoute et du
+            respect des besoins individuels, afin que chaque moment passé ici
+            soit une véritable parenthèse de bien-être.{" "}
+          </p>
+          <p>
+            Merci de me faire confiance pour prendre soin de vous. Contactez-moi
+            pour plus d'informations ou pour réserver votre rendez-vous. J’ai
+            hâte de vous rencontrer et de vous offrir un soin personnalisé, en
+            toute tranquillité et professionnalisme.
+          </p>
+        </div>
       </div>
-
-
 
       {/* Prestations */}
       <section className="home-prestations">
@@ -43,7 +74,6 @@ function home() {
         </div>
       </section>
       <section className="avis-client"></section>
-
     </div>
   );
 }
