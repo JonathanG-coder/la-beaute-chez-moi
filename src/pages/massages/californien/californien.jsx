02931b85bb32e10cf4504aacad4c1847c3d86b1a
Jonathan <jonathangager75@gmail.com>
2025-04-10 13:52:00 +0200

    Add infos at page prenatal

--- src/pages/massages/californien/californien.jsx
+++ src/pages/massages/californien/californien.jsx
@@ -9,30 +9,32 @@ function californien() {
   const tarification = [
     {
       titre: "Le Californien",
-      sousChoix: ["Massage Californien 30 min : 45 €", 
-        "Massage Californien  1h : 90 €"],
-    }
-  ]
+      sousChoix: [
+        "Massage Californien 30 min : 45 €",
+        "Massage Californien  1h : 90 €",
+      ],
+    },
+  ];
   return (
     <div className="container">
-    <Affiche
-      titre={`MASSAGE CALIFORNIEN <br> LA DOUCEUR A PORTER DE MAIN`}
-      sousTitre="L'INSTANT CALIFORNIEN"
-      image={Californien}
-      description={{
-        accroche: "À quoi sert le massage californien ?",
-        details:
-          "Le Californien est le massage sans doute parmi les plus prisés, sans doute car il œuvre des pieds à la tête pour une relaxation et une détente totales, et est particulièrement indiqué pour le mal du siècle : le stress. Créé dans les années 70 par Margaret Elke, il s’inspire de la douceur du massage Esalen qu’elle a combiné avec une démarche plus intérieure et émotive.",
-      }}
-    />
-    {/*  Prestations  */}
-    <Explication
-    titre={"Massage Californien 💆‍♀️"}
-    texte={`Offrez à votre corps une parenthèse de détente avec le massage californien, un soin enveloppant et apaisant qui vise à libérer les tensions et à favoriser une relaxation profonde. Ses mouvements doux et fluides, réalisés avec des huiles parfumées, offrent un véritable voyage sensoriel, permettant de détendre à la fois le corps et l'esprit. Ce massage est idéal pour réduire le stress, améliorer la circulation sanguine et retrouver une sensation de bien-être absolu.`}
-    image={Californien3}
-  />
-  {/*  Tarif  */}
-  <Tarif prestations={tarification} />
+      <Affiche
+        titre={`MASSAGE CALIFORNIEN <br> LA DOUCEUR A PORTER DE MAIN`}
+        sousTitre="L'INSTANT CALIFORNIEN"
+        image={Californien}
+        description={{
+          accroche: "À quoi sert le massage californien ?",
+          details:
+            "Le Californien est le massage sans doute parmi les plus prisés, sans doute car il œuvre des pieds à la tête pour une relaxation et une détente totales, et est particulièrement indiqué pour le mal du siècle : le stress. Créé dans les années 70 par Margaret Elke, il s’inspire de la douceur du massage Esalen qu’elle a combiné avec une démarche plus intérieure et émotive.",
+        }}
+      />
+      {/*  Prestations  */}
+      <Explication
+        titre={"Massage Californien 💆‍♀️"}
+        texte={`Offrez à votre corps une parenthèse de détente avec le massage californien, un soin enveloppant et apaisant qui vise à libérer les tensions et à favoriser une relaxation profonde. Ses mouvements doux et fluides, réalisés avec des huiles parfumées, offrent un véritable voyage sensoriel, permettant de détendre à la fois le corps et l'esprit. Ce massage est idéal pour réduire le stress, améliorer la circulation sanguine et retrouver une sensation de bien-être absolu.`}
+        image={Californien3}
+      />
+      {/*  Tarif  */}
+      <Tarif prestations={tarification} />
     </div>
   );
 }
--- src/pages/massages/prenatal/prenatal.jsx
+++ src/pages/massages/prenatal/prenatal.jsx
@@ -1,19 +1,38 @@
 import "./prenatal.css";
-import Prenatal from "../../../assets/massage/prenatal1.jpg";
+import Prenatal1 from "../../../assets/massage/prenatal1.jpg";
 import Affiche from "../../../components/affiche/affiche";
+import Tarif from "../../../components/tarif/tarif";
+import Explication from "../../../components/explication/explication";
+import Prenatal from "../../../assets/massage/Prenatal.jpg";
 
 function prenatal() {
+  const tarification = [
+    {
+      titre: "Future Maman",
+      sousChoix: ["Massage Femme Enceinte 1h : 80 €"],
+    },
+  ];
   return (
-    <Affiche
-      titre={`MASSAGE PRENATAL POUR VOTRE BEBE & VOUS`}
-      sousTitre="Quand le ventre se fait trop lourd"
-      image={Prenatal}
-      description={{
-        accroche: "À quoi sert le massage prenatal ?",
-        details:
-          " Le massage prénatal est destiné aux femmes enceintes, dès le 4ème mois de grossesse, et les accompagnera jusqu’à leur terme pour leur apporter toute la sérénité nécessaire.",
-      }}
-    />
+    <div className="container">
+      <Affiche
+        titre={`MASSAGE PRENATAL POUR VOTRE BEBE & VOUS`}
+        sousTitre="Quand le ventre se fait trop lourd"
+        image={Prenatal1}
+        description={{
+          accroche: "À quoi sert le massage prenatal ?",
+          details:
+            " Le massage prénatal est destiné aux femmes enceintes, dès le 4ème mois de grossesse, et les accompagnera jusqu’à leur terme pour leur apporter toute la sérénité nécessaire.",
+        }}
+      />
+      {/*  Prestations  */}
+      <Explication
+        titre={"Massage Prenatal 🤰"}
+        texte={`Le massage prénatal est un soin spécialement conçu pour accompagner les futures mamans pendant leur grossesse. Ce massage doux et apaisant aide à soulager les douleurs liées aux changements corporels, à réduire le stress et à favoriser une détente profonde. Grâce à des techniques de massage ciblées, il améliore la circulation sanguine, réduit les tensions musculaires et permet de se reconnecter avec son corps de manière bienveillante. Un moment de sérénité et de bien-être essentiel pour la future maman et son bébé.`}
+        image={Prenatal}
+      />
+      {/*  Tarif  */}
+      <Tarif prestations={tarification} />
+    </div>
   );
 }
 
