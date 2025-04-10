e150461b922b2896cd11f6c8c9351f09b52db5ae
Jonathan <jonathangager75@gmail.com>
2025-04-10 14:56:59 +0200

    Add link to navbar

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,4 +1,3 @@
-// Header.js
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Menu, X } from "lucide-react";
@@ -11,26 +10,58 @@ function Header() {
   return (
     <header>
       <img src={logo} alt="LOGO du site LA BEAUTE CHEZ MOI" />
-      
+
       <nav className="header-menu">
         <ul>
-          <li><Link to="/">Accueil</Link></li>
-          <li><Link to="/prestations">Prestations</Link></li>
-          <li><Link to="/prendre-rendez-vous">Prendre Rendez-vous</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
+          <li>
+            <Link to="/">Accueil</Link>
+          </li>
+          <div class="dropdown">
+            <button class="dropbtn">
+              Prestations
+              <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+              <a href="/manucure">Onglerie</a>
+              <a href="/regard">Beaute du Regard</a>
+              <a href="/massage">Massages</a>
+            </div>
+          </div>
+          <li>
+            <Link to="/prendre-rendez-vous">Prendre Rendez-vous</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
         </ul>
       </nav>
 
       <button className="burger-menu" onClick={() => setIsOpen(!isOpen)}>
         {isOpen ? <X size={30} /> : <Menu size={30} />}
       </button>
-      
+
       <div className={`mobile-menu ${isOpen ? "open" : ""}`}>
         <ul>
-          <li><Link to="/" onClick={() => setIsOpen(false)}>Accueil</Link></li>
-          <li><Link to="/prestations" onClick={() => setIsOpen(false)}>Prestations</Link></li>
-          <li><Link to="/prendre-rendez-vous" onClick={() => setIsOpen(false)}>Prendre Rendez-vous</Link></li>
-          <li><Link to="/contact" onClick={() => setIsOpen(false)}>Contact</Link></li>
+          <li>
+            <Link to="/" onClick={() => setIsOpen(false)}>
+              Accueil
+            </Link>
+          </li>
+          <li>
+            <Link to="/prestations" onClick={() => setIsOpen(false)}>
+              Prestations
+            </Link>
+          </li>
+          <li>
+            <Link to="/prendre-rendez-vous" onClick={() => setIsOpen(false)}>
+              Prendre Rendez-vous
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" onClick={() => setIsOpen(false)}>
+              Contact
+            </Link>
+          </li>
         </ul>
       </div>
     </header>
