3fe7dd92cea61c3410bac1e4a561bf321785e597
Jonathan <jonathangager75@gmail.com>
2025-05-19 15:49:44 +0200

    Modification structure Footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,53 +1,43 @@
 import "../footer/footer.css";
 import logo from "../../assets/logo/Logo-blanc.png";
-
+// ==============TESSSSSTETTETETETE=======
 function Footer() {
+  const year = new Date().getFullYear();
+  
   return (
     <footer>
-      <div className="footer-container">
-        {/* LOGO */}
-        <div className="box1">
-          <img src={logo} alt="LOGO du site LA BEAUTE CHEZ MOI" />
-        </div>
-
-        {/* INFORMATIONS */}
-        <div className="box2">
-          <div className="box2Info">
-            <h3>INFORMATIONS</h3>
-            <a href="/" aria-current="page">
-              Contacter Gabriela
-            </a>
-            <div className="box3">
-              <link
-                rel="stylesheet"
-                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-              />
-              <a href="#" className="fa fa-instagram"></a>
-            </div>
-          </div>
-          {/* LES SOINS */}
-          <div className="box2Lien">
-            <h3>LES SOINS</h3>
-            <div className="LesSoinsInfos">
-              <div className="LesSoinsInfosG">
-                <a href="/" aria-current="page">
-                  Massages
-                </a>
-                <a href="/" aria-current="page">
-                  Onglerie
-                </a>
-                <a href="/" aria-current="page">
-                  Beauté du regard
-                </a>
-              </div>
-
-              {/* COPYRIGHT */}
-              <div className="box4">
-                <p>-Copyright © 2025 LA BEAUTE CHEZ MOI-</p>
-              </div>
-            </div>
-          </div>
-        </div>
+      {/* LOGO */}
+      <div className="boxlogo">
+        <img src={logo} alt="LOGO du site LA BEAUTE CHEZ MOI" />
+      </div>
+      {/* INFORMATIONS */}
+      <div className="boxInfo">
+        <h3>INFORMATIONS</h3>
+        <a href="/" aria-current="page">
+          Contacter Gabriela
+        </a>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        />
+        <a href="#" className="fa fa-instagram"></a>
+      </div>
+      {/* LES SOINS */}
+      <div className="boxSoins">
+        <h3>LES SOINS</h3>
+        <a href="/" aria-current="page">
+          Massages
+        </a>
+        <a href="/" aria-current="page">
+          Onglerie
+        </a>
+        <a href="/" aria-current="page">
+          Beauté du regard
+        </a>
+      </div>
+      {/* COPYRIGHT */}
+      <div className="boxCOPYRIGHT">
+        <p>Copyright © {year} LA BEAUTE CHEZ MOI</p>
       </div>
     </footer>
   );
