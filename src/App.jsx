5f1d9ab99a6986ef458b361e28d4b49f80978b8c
Jonathan <jonathangager75@gmail.com>
2025-05-21 15:36:03 +0200

    Delet Import

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Header from "./components/header/header";
-import Footer from "./components/footer/footer";
-import Home from "./pages/home/home";
-import Manucure from "./pages/manucure/manucure";
-import Regard from "./pages/regard/regard";
-import Massage from "./pages/massages/massage";
-import Rehaussement from "./pages/regard/rehaussement/rehaussement";
-import Extension from "./pages/regard/extension/extension";
-import Sourcils from "./pages/regard/sourcils/sourcils";
-import Prenatal from "./pages/massages/prenatal/prenatal";
-import Californien from "./pages/massages/californien/californien";
-import Tarif from "./components/tarif/tarif";
-import Contact from "./pages/contact/contact";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Manucure from "./pages/manucure/Manucure";
+import Regard from "./pages/regard/Regard";
+import Massage from "./pages/massages/Massage";
+// import Rehaussement from "./pages/regard/rehaussement/Rehaussement";
+// import Extension from "./pages/regard/extension/Extension";
+// import Sourcils from "./pages/regard/sourcils/Sourcils";
+// import Prenatal from "./pages/massages/prenatal/Prenatal";
+// import Californien from "./pages/massages/californien/Californien";
+// import Tarif from "./components/tarif/Tarif";
+import Contact from "./pages/contact/Contact";
 
 function App() {
   return (
@@ -24,12 +24,12 @@ function App() {
           <Route path="/manucure" element={<Manucure />} />
           <Route path="/regard" element={<Regard />} />
           <Route path="/massages" element={<Massage />} />
-          <Route path="/rehaussement" element={<Rehaussement />} />
+          {/* <Route path="/rehaussement" element={<Rehaussement />} />
           <Route path="/extension" element={<Extension />} />
           <Route path="/sourcils" element={<Sourcils />} />
           <Route path="/prenatal" element={<Prenatal />} />
-          <Route path="/californien" element={<Californien />} />
-          <Route path="/tarif" element={<Tarif />} />
+          <Route path="/californien" element={<Californien />} /> */}
+          {/* <Route path="/tarif" element={<Tarif />} /> */}
           <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
