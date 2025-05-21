9c07ed490dc438ffd4d2afd2a2b59d0f77303ede
Jonathan <jonathangager75@gmail.com>
2025-05-21 16:04:23 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Header from "./components/header/Header";
+import Header from "./components/header/HeaderTemp";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
 import Manucure from "./pages/manucure/Manucure";
