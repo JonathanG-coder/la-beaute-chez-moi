b6c5c74e9456a0f97c0f245a1018d23e94d1d671
Jonathan <jonathangager75@gmail.com>
2025-05-28 10:31:59 +0200

    Add new style page contact

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,13 +1,13 @@
 import { useEffect } from "react";
+import { FaMapMarkerAlt, FaPhoneAlt, FaEnvelope } from "react-icons/fa";
 import Contactimg from "../../assets/contact/contactimg.jpg";
 import "./Contact.css";
 import Formulaire from "../../components/formulaire/Formulaire";
 
 export function Contact() {
-
   useEffect(() => {
-      window.scrollTo(0, 0);
-    }, []);
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <>
@@ -17,11 +17,23 @@ export function Contact() {
             <p>Besoin d'aide ?</p>
             <h1>Contactez Gabriela</h1>
             <div className="cont-header-infos">
-              <a href="https://www.google.com/maps/place/405+Chem.+d'Olagainea,+64250+Espelette/@43.3509158,-1.4534263,79m/data=!3m1!1e3!4m15!1m8!3m7!1s0xd513c5e6bcd340b:0x1081eaceab7e67b8!2sChem.+d'Olagainea,+64250+Espelette!3b1!8m2!3d43.3560506!4d-1.4626695!16s%2Fg%2F1td5slnj!3m5!1s0xd513c8f88b3f8f9:0xdec149bfb67f4425!8m2!3d43.3509816!4d-1.453413!16s%2Fg%2F11tc1gnx99?entry=ttu&g_ep=EgoyMDI1MDUyMS4wIKXMDSoASAFQAw%3D%3D">
+              <a
+                href="https://www.google.com/maps/place/405+Chem.+d'Olagainea,+64250+Espelette"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="info-item"
+              >
+                <FaMapMarkerAlt className="info-icon" />
                 365 Olagaineko Bidea, Espelette
               </a>
-              <a href="tel:+33612345678">06 12 34 56 78</a>
-              <a href="mailto:gabygui27@gmail.com">gabygui27@gmail.com</a>
+              <a href="tel:+33612345678" className="info-item">
+                <FaPhoneAlt className="info-icon" />
+                06 12 34 56 78
+              </a>
+              <a href="mailto:gabygui27@gmail.com" className="info-item">
+                <FaEnvelope className="info-icon" />
+                gabygui27@gmail.com
+              </a>
             </div>
           </div>
           <div className="cont-header-img">
@@ -29,10 +41,8 @@ export function Contact() {
           </div>
         </div>
       </div>
-      {/* Formulaire de contact 
-      Le formulaire est importé depuis le composant Formulaire */}
       <div className="formulaire-container">
-      <Formulaire />
+        <Formulaire />
       </div>
     </>
   );
